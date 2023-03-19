645f0efbcfe0e23c16bc79abadce40b503c02d68
Joseph <covasan.iosif7@gmail.com>
2023-03-19 15:58:01 +0200

    draw_move

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ from cell import Cell
 def main():
     win = Window(800, 600)
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c1.draw_move(c2)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     win.wait_for_close()
-    win.mainloop()
 
 
 main()
-
 
