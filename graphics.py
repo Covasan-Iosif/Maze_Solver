7ff53c3de7605a0aa112139ca738c8cd363c98aa
Iosif <covasan.iosif7@gmail.com>
2023-03-19 12:03:49 +0200

    Point,Line,Draw

--- graphics.py
+++ graphics.py
@@ -20,8 +20,31 @@ class Window:
             self.redraw()
         print("Windows closed...")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+        
     def close(self):
         self.__running = False
+
+    
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width = 2
+        )
+        canvas.pack(fill=BOTH, expand=1)
+
+
         
 
     
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800,600)
+    l = Line(Point(50,50), Point(400,400))
+    win.draw_line(l)
     win.wait_for_close()
 
 main()
