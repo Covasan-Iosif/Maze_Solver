80de5542e65d2bf3bd6835e4dad0aa95b15fa66b
Joseph <covasan.iosif7@gmail.com>
2023-03-20 11:43:46 +0200

    break exit and entrace

--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ import random
 import time
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win = None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -14,6 +14,7 @@ class Maze:
         self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
     
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -41,6 +42,12 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0,0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
         
     
---
+++ test.py
@@ -0,0 +1,40 @@
+import unittest
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(m1._cells), num_cols,)
+        self.assertEqual(len(m1._cells[0]), num_rows,)
+
+    def test_maze_create_cells_large(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+        
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            False,
+        )
+        self.assertEqual(
+            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
+        )
+
+    
+if __name__ == "__main__":
+        unittest.main()
